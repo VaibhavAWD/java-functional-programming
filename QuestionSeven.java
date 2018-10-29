bfe3d18ff1f7761e5db43a3c4998c3e47f2fa19f
Vaibhav Dhunde <vaibhavdhunde94@gmail.com>
2018-10-29 18:23:14 +0530

    Add question seven

---
+++ QuestionSeven.java
@@ -0,0 +1,18 @@
+/**
+ * Find the error in this Java code. (Hint: think about scope!)
+ * Imagine that the variables rewinding and playbackPosition could have different values
+ * depending on the situation.
+ */
+public class QuestionSeven {
+    public static void main(String[] args) {
+        double playbackPosition = 120;
+        boolean rewinding = true;
+        
+        if (rewinding) {
+            double rewindAmount = 0.1;
+        }
+        // This line of code may have error as there is no such variable called rewindAmount
+        // It is available only in if statement above
+        playbackPosition = playbackPosition - rewindAmount;
+    }
+}
